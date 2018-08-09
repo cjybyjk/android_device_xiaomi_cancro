ee65c818f0a5960233fa79b6f66114c611536ae0
MkfsSion <myychina28759@gmail.com>
2018-08-09 13:02:30 +0800

    cancro: VNDK: Build libshims_vendor with BOARD_VNDK_VERSION=current
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- libshim/Android.mk
+++ libshim/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := atomic.cpp \
                    dso_handle.cpp
 LOCAL_MODULE := libshims_vendor
+LOCAL_SHARED_LIBRARIES := libcutils
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
